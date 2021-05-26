6ea3828afdeb8f90093799d4626ca0c54d95107d
Alex <alexandre.barczyk@ig2i.centralelille.fr>
2021-05-26 18:07:26 +0200

    Second step: Retrieve Inputs

--- app/src/main/java/com/example/mapremireapplicationavecalexandrequivatresuperbe/FirstFragment.java
+++ app/src/main/java/com/example/mapremireapplicationavecalexandrequivatresuperbe/FirstFragment.java
@@ -1,9 +1,13 @@
 package com.example.mapremireapplicationavecalexandrequivatresuperbe;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -23,11 +27,17 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.idSignIn).setOnClickListener(new View.OnClickListener() {
+        final EditText usernameEditText = view.findViewById(R.id.idUsername);
+        final EditText passwordEditText = view.findViewById(R.id.idPassword);
+        final Button loginButton = view.findViewById(R.id.idSignIn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                Log.i("FRAGMENT 1, username",usernameEditText.getText().toString());
+                Log.i("FRAGMENT 1, password",passwordEditText.getText().toString());
+
+                // NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
